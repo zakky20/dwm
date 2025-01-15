7f5b23f2856e17cd0e2de23d42160302fa1d03b3
Zakky <150591719+zakky20@users.noreply.github.com>
2025-01-15 11:32:05 +0200

    Add files via upload

---
+++ themes/gruvbox.h
@@ -0,0 +1,6 @@
+static char normbgcolor[]           = "#282828";
+static char normbordercolor[]       = "#3c3836";
+static char normfgcolor[]           = "#ebdbb2";
+static char selfgcolor[]            = "#fbf1c7";
+static char selbordercolor[]        = "#282828";
+static char selbgcolor[]            = "#282828";
